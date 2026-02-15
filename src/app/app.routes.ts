8fdbe0a2033632c2292cbd655521af957ea822a9
adalinee <leydyrodriguez985@gmail.com>
2026-02-15 19:34:58 -0300

    Chat-Euphoria-Funcional 1.1, memoria, conversacion con contexto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AnalyticsComponent } from './features/analytics/analytics.component';
 import { WelcomePageComponent } from './features/homePage/pages/home/welcome-page.component';
 import { StandardRegisterComponent } from './features/auth/standar/pages/register/standard-register.component';
 import { StandardLoginComponent } from './features/auth/standar/pages/login/standard-login.component';
+import { ChatComponent } from './features/diary/components/chat.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomePageComponent },
@@ -13,5 +14,6 @@ export const routes: Routes = [
     { path: 'emergency/sent', loadComponent: () => import('./features/emergency/pages/sent/alert-sent.component').then(m => m.AlertSentComponent) },
     { path: 'diario', component: JournalEntryComponent },
     { path: 'analytics', component: AnalyticsComponent },
-    { path: 'analisis', redirectTo: 'analytics' }
+    { path: 'analisis', redirectTo: 'analytics' }, 
+    { path: 'diary/chat', component: ChatComponent }
 ];
