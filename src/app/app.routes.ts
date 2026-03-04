ea92e60a7adcb5632157381e515f807441d3e29c
carlosadso2024 <carlosmario.a@hotmail.com>
2026-03-04 16:23:32 -0500

    creación de la vista para la configuración del contacto de emergencia, conectado con el back

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -68,13 +68,13 @@ export const routes: Routes = [
             import('./features/diary/components/chat.component')
                 .then(m => m.ChatComponent)
     },
-    
+
     {
         path: 'services',
         loadChildren: () =>
             import('./features/services/services.routes')
                 .then(m => m.SERVICES_ROUTES)
-                },
+    },
     {
         path: 'user',
         component: StandardUserLayoutComponent,
@@ -102,6 +102,13 @@ export const routes: Routes = [
                 loadComponent: () =>
                     import('./features/users/standar/settings/settings-account-standar.component')
                         .then(m => m.ConfiguracionComponent)
+            },
+            {
+                path: 'emergency-button',
+                data: { headerText: 'Configuración de Emergencia' },
+                loadComponent: () =>
+                    import('./features/users/standar/emergency-button/emergency-button.component')
+                        .then(m => m.EmergencyButtonComponent)
             }
         ]
     },
