5bd01ee80e2c376189d1356cfed9676c25a0d31d
carlosadso2024 <carlosmario.a@hotmail.com>
2026-03-06 16:31:08 -0500

    integración del dashboard estandar y terapeuta con el backend

--- src/app/layout/therapist-layout/therapist-layout.component.ts
+++ src/app/layout/therapist-layout/therapist-layout.component.ts
@@ -32,9 +32,8 @@ export class TherapistLayoutComponent {
 
   navItems = [
     { label: 'Inicio', icon: 'home', route: '/users/therapist/dashboard' },
-    { label: 'Notificaciones', icon: 'notificacion', route: '/users/therapist/notifications' },
     { label: 'Pacientes', icon: 'pacientes', route: '/users/therapist/patients' },
-    { label: 'Servicios', icon: 'servicio', route: '/users/therapist/services' },
+    { label: 'Servicios', icon: 'servicio', route: '/services' },
     { label: 'Perfil', icon: 'usuario', route: '/users/therapist/profile' }
   ];
 
