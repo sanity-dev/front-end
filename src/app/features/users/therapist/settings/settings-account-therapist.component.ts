2082ced542615ab14a5f6b5906b835a3fa99e74e
Czuluaga16 <carlosandreszuluagaatehortua@gmail.com>
2026-03-24 21:03:05 -0500

    eliminate logout button from therapist settings

--- src/app/features/users/therapist/settings/settings-account-therapist.component.ts
+++ src/app/features/users/therapist/settings/settings-account-therapist.component.ts
@@ -52,16 +52,6 @@ import { environment } from '../../../../../environments/environment';
 
         <div class="flex flex-col gap-2">
           <app-settings-item
-            label="Cerrar Sesión"
-            type="danger"
-            (itemClick)="logout()"
-          >
-            <svg icon xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-            </svg>
-          </app-settings-item>
-
-          <app-settings-item
             label="Eliminar Cuenta"
             type="danger"
             (itemClick)="confirmDeleteAccount()"
